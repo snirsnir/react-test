43dec4be808957fe097166cb20afbbe978311794
unknown <papasnir@gmail.com>
2021-06-16 01:10:14 +0300

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to my website</h1>
+      <h2>great!</h2>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Aubergine.jpg/250px-Aubergine.jpg"
+      alt="new"/>
+    </div>
+  );
+}
+
+export default App;

4665e52dac50722898e7d50e531a3cc1ca936e90
unknown <papasnir@gmail.com>
2021-06-16 01:19:49 +0300

    tomato

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1>welcome to my website</h1>
       <h2>great!</h2>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Aubergine.jpg/250px-Aubergine.jpg"
+      <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Tomato_je.jpg"
       alt="new"/>
     </div>
   );
